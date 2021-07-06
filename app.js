0b8d2466e7bc0d78c9a03a94e63671c8ce67f01a
Simon Kaine <simonkaine@gmail.com>
2021-07-06 14:29:52 -0700

    structure + first product outline + png images

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 // import functions and grab DOM elements
-
+import { renderCharcter } from './render-characters.js';
 // initialize state
 
 // set event listeners 
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+
---
+++ characters.js
@@ -0,0 +1,8 @@
+const mario = {
+    id: 'mario',
+    name: 'Mario',
+    image: 'mario.png',
+    description: "It's a me, Mario!",
+    category: 'Super Mario bros',
+    price: 100.00
+};
