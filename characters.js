b9df1b00911560d854c71f1f0359578319b78053
Simon Kaine <simonkaine@gmail.com>
2021-07-06 14:48:20 -0700

    Product data + Object literals + Array Variable

--- characters.js
+++ characters.js
@@ -4,5 +4,71 @@ const mario = {
     image: 'mario.png',
     description: "It's a me, Mario!",
     category: 'Super Mario bros',
+    price: 99.00
+};
+
+const data = {
+    id: 'data',
+    name: 'Data',
+    image: 'data.png',
+    description: "I have no culture of my own.",
+    category: 'Star Trek',
     price: 100.00
 };
+
+const donkeyKong = {
+    id: 'donkey-kong',
+    name: 'Donkey Kong',
+    image: 'donkeyKong.png',
+    description: 'Banana slamma!!',
+    category: 'Donkey Kong',
+    price: 95.00
+};
+
+const earthwormJim = {
+    id: 'earthworm-jim',
+    name: 'Eartworm Jim',
+    image: 'earthworm-jim.png',
+    description: 'Groovy!',
+    category: 'Earthworm Jim',
+    price: 90.00
+};
+
+const ghost = {
+    id: 'ghost',
+    name: 'Ghost',
+    image: 'ghost.png',
+    description: 'BOO!',
+    category: 'Super Mario bros',
+    price: 77.00
+};
+
+const scorpion = {
+    id: 'scorpion',
+    name: 'Scorpion',
+    image: 'scorpion.png',
+    description: 'Get over here!',
+    category: 'Mortal Kombat',
+    price: 95.00
+};
+
+const subZero = {
+    id: 'sub-zero',
+    name: 'Sub-Zero',
+    image: 'sub-zero.png',
+    description: 'I am Sub-Zero, not Captain Cold!',
+    category: 'Mortal Kombat',
+    price: 95.00
+};
+
+const characters = [
+    mario, 
+    data, 
+    donkeyKong,
+    earthwormJim,
+    ghost,
+    scorpion,
+    subZero
+];
+
+export default characters;
