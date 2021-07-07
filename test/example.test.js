bef9f2f95041601fe2c1ddb1d33a5619a765d3c9
Simon Kaine <simonkaine@gmail.com>
2021-07-07 15:45:24 -0700

    TDD calcItemTotal Function

--- test/example.test.js
+++ test/example.test.js
@@ -1,5 +1,5 @@
 import { renderCharacter } from '../render-characters.js';
-import { findById } from '../utils.js';
+import { findById, calcItemTotal } from '../utils.js';
 
 const test = QUnit.test;
 
@@ -36,6 +36,31 @@ test('Test for two objects comparrison.', (expect) => {
     //Act 
     // Call the function you're testing and set the result to a const
     const actual = findById(mario, 1);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.deepEqual(actual, expected);
+});
+
+test('test for calculation', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = 297;
+    const mario = [{
+        id: 1,
+        name: 'Mario',
+        image: 'mario.png',
+        description: "It's a me, Mario!",
+        category: 'Super Mario bros',
+        price: 99.00
+    }];
+
+    const cart = [
+        { id: 1, qty: 3 }
+    ];
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = calcItemTotal(mario, cart);
 
     //Expect
     // Make assertions about what is expected versus the actual result
--- utils.js
+++ utils.js
@@ -5,3 +5,13 @@ export function findById(arrayItem, idItem) {
         }
     }
 }
+//  do i want arrayItem qty? idItem or total?
+export function calcItemTotal(arrayItem, idItem) {
+    let total = 0;
+    for (let item of idItem) {
+        const character = findById(arrayItem, item.id);
+        total += character.price * item.qty;
+    }
+    return total;
+}
+
